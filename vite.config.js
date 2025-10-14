1d206220322e7d40c5cd942eadf5a23ea26818f8
mrnia204 <204codetribe@gmail.com>
2025-10-15 09:39:22 +1000

    vite-config added

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
+
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [
     react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // source file
+          dest: '.'                 // copy to root of dist
+        }
+      ]
+    })
   ],
   build: {
     minify: 'esbuild',
     sourcemap: false,
   }
+})
 
-});
